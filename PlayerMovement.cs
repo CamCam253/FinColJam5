454db8c295a81521ed8c3e231aa91bbef9bb9622
CamCam253 <camiselkala@gmail.com>
2023-10-09 14:45:50 +0300

    Added Script for basic movement

---
+++ PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2f;
+    private Rigidbody2D player;
+    private Vector2 movementDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        player.velocity = movementDirection * movementSpeed;
+    }
+}
